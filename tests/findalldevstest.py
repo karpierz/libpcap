9e1dd9e11cc58847528dc81894da5df095f977f2
Adam Karpierz <akarpie@ra.rockwell.com>
2017-08-18 10:04:40 +0200

    ...

--- tests/findalldevstest.py
+++ tests/findalldevstest.py
@@ -45,7 +45,7 @@ def main():
     alldevs = ct.POINTER(pcap.pcap_if_t)()
     errbuf  = ct.create_string_buffer(pcap.PCAP_ERRBUF_SIZE + 1)
     if pcap.findalldevs(ct.byref(alldevs), errbuf) == -1:
-        print("Error in pcap.findalldevs: {!s}".format(errbuf.value), file=sys.stderr)
+        print("Error in pcap.findalldevs: {!s}".format(errbuf.value.decode("utf-8")), file=sys.stderr)
         sys.exit(1)
 
     d = alldevs
@@ -56,14 +56,14 @@ def main():
 
     s = pcap.lookupdev(errbuf)
     if s is None:
-        print("Error in pcap.lookupdev: {!s}".format(errbuf.value), file=sys.stderr)
+        print("Error in pcap.lookupdev: {!s}".format(errbuf.value.decode("utf-8")), file=sys.stderr)
     else:
         print("Preferred device name: {!s}".format(s.decode("utf-8")))
 
     net  = pcap.bpf_u_int32()
     mask = pcap.bpf_u_int32()
     if pcap.lookupnet(s, ct.byref(net), ct.byref(mask), errbuf) < 0:
-        print("Error in pcap.lookupnet: {!s}".format(errbuf.value), file=sys.stderr)
+        print("Error in pcap.lookupnet: {!s}".format(errbuf.value.decode("utf-8")), file=sys.stderr)
     else:
         print("Preferred device is on network: {}/{}".format(iptos(net), iptos(mask)))
     
@@ -75,7 +75,7 @@ def ifprint(d): # pcap_if_t*
     print("{!s}".format(d.name.decode("utf-8")))
     if d.description:
         print("\tDescription: {!s}".format(d.description.decode("utf-8")))
-    print("\tLoopback: {!s}".format("yes" if d.flags & pcap.PCAP_IF_LOOPBACK else "no"))
+    print("\tLoopback: {}".format("yes" if d.flags & pcap.PCAP_IF_LOOPBACK else "no"))
 
     a = d.addresses
     while a:
@@ -88,24 +88,24 @@ def ifprint(d): # pcap_if_t*
         if addr.contents.sa_family == socket.AF_INET:
             print("\tAddress Family: AF_INET")
             if addr:
-                print("\t\tAddress: {!s}".format(socket.inet_ntoa(ct.cast(addr, ct.POINTER(sockaddr_in)).contents.sin_addr)))
+                print("\t\tAddress: {}".format(socket.inet_ntoa(ct.cast(addr, ct.POINTER(sockaddr_in)).contents.sin_addr)))
             if netmask:
-                print("\t\tNetmask: {!s}".format(socket.inet_ntoa(ct.cast(netmask, ct.POINTER(sockaddr_in)).contents.sin_addr)))
+                print("\t\tNetmask: {}".format(socket.inet_ntoa(ct.cast(netmask, ct.POINTER(sockaddr_in)).contents.sin_addr)))
             if broadaddr:
-                print("\t\tBroadcast Address: {!s}".format(socket.inet_ntoa(ct.cast(broadaddr, ct.POINTER(sockaddr_in)).contents.sin_addr)))
+                print("\t\tBroadcast Address: {}".format(socket.inet_ntoa(ct.cast(broadaddr, ct.POINTER(sockaddr_in)).contents.sin_addr)))
             if dstaddr:
-                print("\t\tDestination Address: {!s}".format(socket.inet_ntoa(ct.cast(dstaddr, ct.POINTER(sockaddr_in)).contents.sin_addr)))
+                print("\t\tDestination Address: {}".format(socket.inet_ntoa(ct.cast(dstaddr, ct.POINTER(sockaddr_in)).contents.sin_addr)))
         #ifdef INET6
         elif addr.contents.sa_family == socket.AF_INET6:
             print("\tAddress Family: AF_INET6")
             if addr:
-                print("\t\tAddress: {!s}".format(socket.inet_ntop(socket.AF_INET6, ct.cast(addr, ct.POINTER(sockaddr_in6)).contents.sin6_addr.s6_addr)))
+                print("\t\tAddress: {}".format(socket.inet_ntop(socket.AF_INET6, ct.cast(addr, ct.POINTER(sockaddr_in6)).contents.sin6_addr.s6_addr)))
             if netmask:
-                print("\t\tNetmask: {!s}".format(socket.inet_ntop(socket.AF_INET6, ct.cast(netmask, ct.POINTER(sockaddr_in6)).contents.sin6_addr.s6_addr)))
+                print("\t\tNetmask: {}".format(socket.inet_ntop(socket.AF_INET6, ct.cast(netmask, ct.POINTER(sockaddr_in6)).contents.sin6_addr.s6_addr)))
             if broadaddr:
-                print("\t\tBroadcast Address: {!s}".format(socket.inet_ntop(socket.AF_INET6, ct.cast(broadaddr, ct.POINTER(sockaddr_in6)).contents.sin6_addr.s6_addr)))
+                print("\t\tBroadcast Address: {}".format(socket.inet_ntop(socket.AF_INET6, ct.cast(broadaddr, ct.POINTER(sockaddr_in6)).contents.sin6_addr.s6_addr)))
             if dstaddr:
-                print("\t\tDestination Address: {!s}".format(socket.inet_ntop(socket.AF_INET6, ct.cast(dstaddr, ct.POINTER(sockaddr_in6)).contents.sin6_addr.s6_addr)))
+                print("\t\tDestination Address: {}".format(socket.inet_ntop(socket.AF_INET6, ct.cast(dstaddr, ct.POINTER(sockaddr_in6)).contents.sin6_addr.s6_addr)))
         #endif
         else:
             print("\tAddress Family: Unknown ({:d})".format(addr.contents.sa_family))

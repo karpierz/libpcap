f9e14033999d46930436d787ff577933e36139ee
Adam Karpierz <akarpierz@gmail.com>
2017-08-20 20:31:07 +0200

    ...

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

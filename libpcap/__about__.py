98e43f6d72d3412f9dd9f575bb317491341b58f3
Adam Karpierz <akarpierz@gmail.com>
2017-08-27 11:49:12 +0200

    1.0.0b1

--- libpcap/__about__.py
+++ libpcap/__about__.py
@@ -7,8 +7,8 @@ __all__ = ('__title__', '__summary__', '__uri__', '__version_info__',
 __title__        = "libpcap"
 __summary__      = "Python binding for the libpcap C library (ctypes/cffi-based libpcap)"
 __uri__          = "http://pypi.python.org/pypi/libpcap/"
-__version_info__ = type("version_info", (), dict(serial=16,
-                        major=1, minor=0, micro=0, releaselevel="alpha"))
+__version_info__ = type("version_info", (), dict(serial=1,
+                        major=1, minor=0, micro=0, releaselevel="beta"))
 __version__      = "{0.major}.{0.minor}.{0.micro}{1}{2}".format(__version_info__,
                    dict(final="", alpha="a", beta="b", rc="rc")[__version_info__.releaselevel],
                    "" if __version_info__.releaselevel == "final" else __version_info__.serial)

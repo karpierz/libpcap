2f366c0e04ffa4b0c2dbbcfa2b068f43a5712368
Adam Karpierz <akarpie@ra.rockwell.com>
2017-10-05 20:01:57 +0200

    ...

--- libpcap/__config__.py
+++ libpcap/__config__.py
@@ -5,27 +5,20 @@
 from __future__ import absolute_import
 
 
-def get_config(cfg_path):
-
-    import os.path as osp
-    from runpy import run_path
-
-    return ({k: v for k, v in run_path(cfg_path).items() if not k.startswith("__")}
-            if osp.isfile(cfg_path) else {})
-
-
 def make_config(cfg_name):
 
     import sys
     import os.path as osp
+    from runpy import run_path
 
     fglobals = sys._getframe(1).f_globals
+    fglobals.pop("make_config", None)
+    print(fglobals)
     cfg_path = osp.join(osp.dirname(fglobals["__file__"]), cfg_name)
-    cfg_dict = get_config(cfg_path)
-    fglobals.update(cfg_dict)
-    fglobals.pop("__builtins__", None)
-    fglobals.pop("__cached__",   None)
-    fglobals["__all__"] = tuple(cfg_dict.keys())
+    if osp.isfile(cfg_path):
+        fglobals.update({k: v for k, v in run_path(cfg_path).items()
+                         if not k.startswith("__")})
+    print(fglobals)
 
 
 make_config("libpcap.cfg")
